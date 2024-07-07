1d218e5bf76a7166b9f2e8287589acc7a32a5fa7
Explorarthecreator <fredadejiemma99@gmail.com>
2024-07-07 05:02:08 +0100

    Created the navbar for the mobile screen and the cart stae to handle evry state update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,38 @@
 
+import logo from '../images/Timbu.svg'
+import cartLogo from '../images/cart.svg'
+import hamburger from '../images/hamburger.svg'
+import heart from '../images/heart.svg'
+import account from '../images/account.svg'
+import { useSelector } from 'react-redux'
 
 function Navbar() {
+  const {cart} = useSelector((state)=>state.cart)
   return (
-    <div>Navbar</div>
+    <div className=' bg-[#C6BDDE] flex items-center justify-between px-3 py-5'>
+      <img src={logo} alt="Timbu Logo" />
+      <div className='flex gap-4'>
+        <div className='flex gap-2 items-center'>
+          <div>
+            <img src={account} alt="" />
+          </div>
+          <div>
+            <img src={heart} alt="" />
+          </div>
+          <div className='indicator'>
+            <img src={cartLogo} alt="" />
+            <span className="badge badge-xs border-none text-white bg-black indicator-item">
+              {
+                cart.length
+              }
+            </span>
+          </div>
+        </div>
+        <div>
+          <img src={hamburger} alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+
+const cart = JSON.parse(localStorage.getItem('cart'))
+
+const initialState = {
+    cart : cart? cart :[]
+}
+
+const CartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers:{
+        reset:(state)=>initialState,
+        addToCart:(state,action)=>{
+            const newCartitem = action.payload
+            
+            const exist = state.cart.find((cartItem)=> cartItem.id === newCartitem.id)
+
+            if(exist){
+                toast.error('Item already exists in cart')
+            }else{
+                let oldItems
+
+                if(state.cart.length <=0){
+                    oldItems = [newCartitem]
+                }else{
+                    oldItems = [...state.cart,newCartitem]
+                }
+
+                localStorage.setItem('cart',JSON.stringify(oldItems))
+                state.cart.push(newCartitem)
+                toast.success('Added to cart')
+            }
+        }
+    }
+})
+
+export const {reset, addToCart} = CartSlice.actions
+
+export default CartSlice.reducer
