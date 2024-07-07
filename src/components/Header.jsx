df3c01c95441fae5b06d7a59307c7a94500bda60
Explorarthecreator <fredadejiemma99@gmail.com>
2024-07-07 05:05:41 +0100

    Created the header and footer component and it is fully responsive on hte vbarious screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,30 @@
-
+import { useLocation } from 'react-router-dom'
+import header from '../images/header.jpg'
 
 function Header() {
+  const location = useLocation()
+
+  const pathMatchRoute = (route)=>{
+    if(route === location.pathname){
+      return true
+    }
+  }
   return (
-    <div>Header</div>
+    <div className='font-poppins font-bold h-12 md:h-16 lg:h-28 w-full relative flex items-center justify-center'>
+      
+      <h1>
+        {
+          pathMatchRoute('/') && 'Shop all our products'
+        }
+        {
+          pathMatchRoute('/cart') && 'Cart'
+        }
+        {
+          pathMatchRoute('/checkout') && 'Checkout'
+        }
+      </h1>
+      <img src={header} alt="" className='w-full h-full absolute left-0 top-0 -z-10' />
+    </div>
   )
 }
 
