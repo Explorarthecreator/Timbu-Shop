8d864c14f108d2480d03e8b106271e84388304bf
Explorarthecreator <fredadejiemma99@gmail.com>
2024-07-07 05:07:02 +0100

    registered all the redux state

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import productReducer from '../features/product/productSlice'
+import wishlistReducer from '../features/wishlist/wishList'
+import cartReducer from '../features/cart/cartSlice'
 
 export const store = configureStore({
   reducer: {
-    
+    product:productReducer,
+    wishlist:wishlistReducer,
+    cart: cartReducer
   },
 });
